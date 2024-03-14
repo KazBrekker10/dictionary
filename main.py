e6d65a6f2aa6c9f82934c34a8aa619ce880b363e
KazBrekker10 <137906675+KazBrekker10@users.noreply.github.com>
2024-03-14 23:49:06 +0000

    actualizando buscador de terminos

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-animal = 'perro'
-print(animal)
+import json
+def search (keyword):
+  with open ('index.json', "r") as file:
+    words = json.load(file)
+    for word in words:
+      if keyword in word['keywords']:
+        return word
+      else:
+        print('no se encontro el termino en le indice')
+
