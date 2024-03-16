6ba99073d71ba012c30bd0b694eb5bcb20e716fe
KazBrekker10 <137906675+KazBrekker10@users.noreply.github.com>
2024-03-16 16:23:23 -0500

    Agregando insectos

--- words.py
+++ words.py
@@ -1,4 +1,5 @@
 words = {
 "canino" : ["perro", "lobo", "zorro", "chacal", "coyote", "hiena"],
-"aves" : ["paloma", "aguila", "halcon", "canario", "loro", "ruiseñor", "colibri", "condor"]
+"aves" : ["paloma", "aguila", "halcon", "canario", "loro", "ruiseñor", "colibri", "condor"],
+"insectos" : ["moscas", "cucarachas", "hormigas", "mariposas"]
 }
